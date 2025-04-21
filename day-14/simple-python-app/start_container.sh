324ef1d8b1efb48a00f537ccf5c968a5d15d1e82
kaiz1994 <141048374+kaiz1994@users.noreply.github.com>
2025-04-21 22:16:20 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling Docker Image"
+docker pull kaizjessani/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 simple-python-flask-app:latest
